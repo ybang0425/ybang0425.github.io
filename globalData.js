26bd4c9bc81c8e8d3fa19dc897a925871ca03c34
ybang0425 <sean3124@naver.com>
2025-07-19 21:01:39 +0900

    clean

--- globalData.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';  // fs 모듈을 가져옵니다.
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-
-// __dirname 대체 구현
-const __filename = fileURLToPath(import.meta.url); // 현재 파일 경로
-const __dirname = dirname(__filename); // 현재 디렉토리 경로
-
